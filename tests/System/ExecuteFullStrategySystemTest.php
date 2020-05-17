db13253fdf8d6623152f160e8ee6c8a9884b296a
Pablo-Lilao <pablolilao@protonmail.com>
2020-05-17 10:14:59 +0200

    Fixed testcases incompatibility issues

--- tests/System/ExecuteFullStrategySystemTest.php
+++ tests/System/ExecuteFullStrategySystemTest.php
@@ -3,8 +3,10 @@
 namespace Tests\System;
 
 use GitHooks\GitHooks;
+use GitHooks\Tools\CheckSecurity;
 use Illuminate\Container\Container;
 use Tests\SystemTestCase;
+
 
 /**
  * No se tiene encuenta check-security ya que depende de la configuración de composer.json y de que no se hayan encontrado
@@ -13,11 +15,11 @@ use Tests\SystemTestCase;
 class ExecuteFullStrategySystemTest extends SystemTestCase
 {
     //Allgoritmo de todos los pares:
-    // Check Security   Code Sniffer    Mess Detector CPDectector   Parallellint    Stan
+    // Check Security   Mess Detector   Code Sniffer  Parallellint  CPDectector       Stan
     // OK               OK              OK          OK              OK              OK      --> it_execute_all_tools_and_pass_all_checks
     // OK               KO              KO          KO              KO              KO      --> it_execute_all_tools_all_ko
-    // KO               KO              OK          KO              OK              KO      --> it_execute_all_tools_cpd_and_parallellint_ok
-    // KO               OK              KO          OK              KO              OK      --> 
+    // KO               KO              OK          KO              OK              KO      --> it_execute_all_tools_codesniffer_and_copypastedetector_ok
+    // KO               OK              KO          OK              KO              OK      --> it_execute_all_tools_codesniffer_and_copypastedetector_ko
 
     protected function setUp(): void
     {
@@ -45,6 +47,7 @@ class ExecuteFullStrategySystemTest extends SystemTestCase
         file_put_contents($this->getPath() . '/src/File.php', $fileBuilder->build());
 
         $container = Container::getInstance();
+        $container->bind(CheckSecurity::class, CheckSecurityFakeOk::class);
         $githooks = $container->makeWith(GitHooks::class, ['configFile' => $this->getPath() . '/githooks.yml']);
 
         try {
@@ -53,12 +56,12 @@ class ExecuteFullStrategySystemTest extends SystemTestCase
             //Si algo sale mal evito lanzar la excepcion porque oculta los asserts
         }
 
-        $regExp = '(\.phar)? - OK\. Time: \d+\.\d{2}'; //phpcbf[.phar] - OK. Time: 0.18
-        $this->assertRegExp("%phpcbf$regExp%", $this->getActualOutput());
-        $this->assertRegExp("%phpmd$regExp%", $this->getActualOutput());
-        $this->assertRegExp("%phpcpd$regExp%", $this->getActualOutput());
-        $this->assertRegExp("%phpstan$regExp%", $this->getActualOutput());
-        $this->assertRegExp("%parallel-lint$regExp%", $this->getActualOutput());
+
+        $this->assertToolHasBeenExecutedSuccessfully('phpcbf');
+        $this->assertToolHasBeenExecutedSuccessfully('phpmd');
+        $this->assertToolHasBeenExecutedSuccessfully('phpcpd');
+        $this->assertToolHasBeenExecutedSuccessfully('phpstan');
+        $this->assertToolHasBeenExecutedSuccessfully('parallel-lint');
         $this->assertRegExp('%Tiempo total de ejecución = \d+\.\d{2} sec%', $this->getActualOutput());
         $this->assertStringContainsString('Tus cambios se han commiteado.', $this->getActualOutput());
     }
@@ -66,7 +69,6 @@ class ExecuteFullStrategySystemTest extends SystemTestCase
     /** @test */
     function it_execute_all_tools_all_ko()
     {
-        $this->markTestIncomplete('make phpcpd fail in test case (add phpcpd error in PhpFileBuilder buildWithErrors)');
         $fileBuilder = new PhpFileBuilder('File');
 
         $configurationFileBuilder = new ConfigurationFileBuilder($this->getPath());
@@ -75,11 +77,11 @@ class ExecuteFullStrategySystemTest extends SystemTestCase
 
 
         file_put_contents($this->getPath() . '/src/File.php', $fileBuilder->buildWithErrors([
-            PhpFileBuilder::PHPMD, PhpFileBuilder::PHPCS, PhpFileBuilder::PHPCS_NO_FIXABLE, PhpFileBuilder::PHPSTAN, PhpFileBuilder::PARALLEL_LINT,
+            PhpFileBuilder::PHPMD, PhpFileBuilder::PHPCS, PhpFileBuilder::PHPCS_NO_FIXABLE, PhpFileBuilder::PHPSTAN, PhpFileBuilder::PARALLEL_LINT, PhpFileBuilder::PHPCPD
         ]));
-        //file_put_contents($this->getPath() . '/src/File.php', $fileBuilder->buildWithErrors());
 
         $container = Container::getInstance();
+        $container->bind(CheckSecurity::class, CheckSecurityFakeOk::class);
         $githooks = $container->makeWith(GitHooks::class, ['configFile' => $this->getPath() . '/githooks.yml']);
 
         try {
@@ -88,20 +90,17 @@ class ExecuteFullStrategySystemTest extends SystemTestCase
             //Si algo sale mal evito lanzar la excepcion porque oculta los asserts
         }
 
-        $regExp = '(\.phar)? - KO\. Time: \d+\.\d{2}'; //phpcbf[.phar] - OK. Time: 0.18
-
-        //TODO PABLO: make phpcpd fail in test case (add phpcpd error in PhpFileBuilder buildWithErrors)
-        $this->assertRegExp("%phpcbf$regExp%", $this->getActualOutput());
-        $this->assertRegExp("%phpmd$regExp%", $this->getActualOutput());
-        $this->assertRegExp("%phpcpd$regExp%", $this->getActualOutput());
-        $this->assertRegExp("%phpstan$regExp%", $this->getActualOutput());
-        $this->assertRegExp("%parallel-lint$regExp%", $this->getActualOutput());
+        $this->assertToolHasFailed('phpcbf');
+        $this->assertToolHasFailed('phpmd');
+        $this->assertToolHasFailed('phpcpd');
+        $this->assertToolHasFailed('phpstan');
+        $this->assertToolHasFailed('parallel-lint');
         $this->assertRegExp('%Tiempo total de ejecución = \d+\.\d{2} sec%', $this->getActualOutput());
         $this->assertStringContainsString('Tus cambios no se han commiteado. Por favor, corrige los errores y vuelve a intentarlo.', $this->getActualOutput());
     }
 
     /** @test */
-    function it_execute_all_tools_cpd_and_parallellint_ok()
+    function it_execute_all_tools_codesniffer_and_copypastedetector_ok()
     {
         $fileBuilder = new PhpFileBuilder('File');
 
@@ -111,11 +110,12 @@ class ExecuteFullStrategySystemTest extends SystemTestCase
 
 
         file_put_contents($this->getPath() . '/src/File.php', $fileBuilder->buildWithErrors([
-            PhpFileBuilder::PHPMD, PhpFileBuilder::PHPCS, PhpFileBuilder::PHPCS_NO_FIXABLE, PhpFileBuilder::PHPSTAN,
+            PhpFileBuilder::PARALLEL_LINT, PhpFileBuilder::PHPMD, PhpFileBuilder::PHPSTAN,
         ]));
-        //file_put_contents($this->getPath() . '/src/File.php', $fileBuilder->buildWithErrors());
 
         $container = Container::getInstance();
+        //TODO Pablo: mockear CheckSecurityFakeKO
+        $container->bind(CheckSecurity::class, CheckSecurityFakeOk::class);
         $githooks = $container->makeWith(GitHooks::class, ['configFile' => $this->getPath() . '/githooks.yml']);
 
         try {
@@ -124,21 +124,17 @@ class ExecuteFullStrategySystemTest extends SystemTestCase
             //Si algo sale mal evito lanzar la excepcion porque oculta los asserts
         }
 
-        $regExpKO = '(\.phar)? - KO\. Time: \d+\.\d{2}';
-        $regExpOk = '(\.phar)? - OK\. Time: \d+\.\d{2}';
-
-        //TODO PABLO: make phpcpd fail in test case (add phpcpd error in PhpFileBuilder buildWithErrors)
-        $this->assertRegExp("%phpcbf$regExpKO%", $this->getActualOutput());
-        $this->assertRegExp("%phpmd$regExpKO%", $this->getActualOutput());
-        $this->assertRegExp("%phpcpd$regExpOk%", $this->getActualOutput());
-        $this->assertRegExp("%phpstan$regExpKO%", $this->getActualOutput());
-        $this->assertRegExp("%parallel-lint$regExpOk%", $this->getActualOutput());
+        $this->assertToolHasFailed(PhpFileBuilder::PHPSTAN);
+        $this->assertToolHasFailed(PhpFileBuilder::PHPMD);
+        $this->assertToolHasFailed(PhpFileBuilder::PARALLEL_LINT);
+        $this->assertToolHasBeenExecutedSuccessfully(PhpFileBuilder::PHPCPD);
+        $this->assertToolHasBeenExecutedSuccessfully('phpcbf');
         $this->assertRegExp('%Tiempo total de ejecución = \d+\.\d{2} sec%', $this->getActualOutput());
         $this->assertStringContainsString('Tus cambios no se han commiteado. Por favor, corrige los errores y vuelve a intentarlo.', $this->getActualOutput());
     }
 
     /** @test */
-    function it_execute_all_tools_cpd_and_parallellint_ko()
+    function it_execute_all_tools_codesniffer_and_copypastedetector_ko()
     {
         $fileBuilder = new PhpFileBuilder('File');
 
@@ -148,11 +144,13 @@ class ExecuteFullStrategySystemTest extends SystemTestCase
 
 
         file_put_contents($this->getPath() . '/src/File.php', $fileBuilder->buildWithErrors([
-            PhpFileBuilder::PARALLEL_LINT
+            PhpFileBuilder::PHPCS_NO_FIXABLE, PhpFileBuilder::PHPCS, PhpFileBuilder::PHPCPD
         ]));
         //file_put_contents($this->getPath() . '/src/File.php', $fileBuilder->buildWithErrors());
 
         $container = Container::getInstance();
+        //TODO Pablo: mockear CheckSecurityFakeKO
+        $container->bind(CheckSecurity::class, CheckSecurityFakeOk::class);
         $githooks = $container->makeWith(GitHooks::class, ['configFile' => $this->getPath() . '/githooks.yml']);
 
         try {
@@ -161,52 +159,13 @@ class ExecuteFullStrategySystemTest extends SystemTestCase
             //Si algo sale mal evito lanzar la excepcion porque oculta los asserts
         }
 
-        $regExpKO = '(\.phar)? - KO\. Time: \d+\.\d{2}';
-        $regExpOk = '(\.phar)? - OK\. Time: \d+\.\d{2}';
-
-        //TODO PABLO: make phpcpd fail in test case (add phpcpd error in PhpFileBuilder buildWithErrors)
-        // $this->assertRegExp("%phpcbf$regExpOk%", $this->getActualOutput());
-        $this->assertRegExp("%phpmd$regExpOk%", $this->getActualOutput());
-        // $this->assertRegExp("%phpcpd$regExpOk%", $this->getActualOutput());
-        // $this->assertRegExp("%phpstan$regExpOk%", $this->getActualOutput());
-        // $this->assertRegExp("%parallel-lint$regExpKO%", $this->getActualOutput());
+        $this->assertToolHasBeenExecutedSuccessfully(PhpFileBuilder::PHPSTAN);
+        $this->assertToolHasBeenExecutedSuccessfully(PhpFileBuilder::PHPMD);
+        $this->assertToolHasBeenExecutedSuccessfully(PhpFileBuilder::PARALLEL_LINT);
+        $this->assertToolHasFailed('phpcbf');
+        $this->assertToolHasFailed(PhpFileBuilder::PHPCPD);
         $this->assertRegExp('%Tiempo total de ejecución = \d+\.\d{2} sec%', $this->getActualOutput());
         $this->assertStringContainsString('Tus cambios no se han commiteado. Por favor, corrige los errores y vuelve a intentarlo.', $this->getActualOutput());
-    }
-
-    /** @test */
-    function it_otro_caso()
-    {
-        $this->markTestIncomplete('Hay que corregir PHPMD cuando hay un error de sintaxis y PHPSTAN que no falla nunca');
-        $fileBuilder = new PhpFileBuilder('File');
-
-        $configurationFileBuilder = new ConfigurationFileBuilder($this->getPath());
-
-        // $configurationFile = $configurationFileBuilder->buildYalm();
-        file_put_contents($this->getPath() . '/githooks.yml', $configurationFileBuilder->buildYalm());
-
-        file_put_contents($this->getPath() . '/src/File.php', $fileBuilder->buildWithErrors([
-            PhpFileBuilder::PHPMD, PhpFileBuilder::PHPCS, PhpFileBuilder::PHPCS_NO_FIXABLE, PhpFileBuilder::PHPSTAN, PhpFileBuilder::PARALLEL_LINT,
-        ]));
-
-        $container = Container::getInstance();
-        $githooks = $container->makeWith(GitHooks::class, ['configFile' => $this->getPath() . '/githooks.yml']);
-
-        $githooks();
-
-        $regExp = '(\.phar)? - KO\. Time: \d+\.\d{2}'; //phpcbf[.phar] - OK. Time: 0.18
-        $this->assertRegExp("%phpcbf$regExp%", $this->getActualOutput());
-        $this->assertRegExp("%phpmd$regExp%", $this->getActualOutput());
-        $this->assertRegExp("%phpcpd$regExp%", $this->getActualOutput());
-        $this->assertRegExp("%phpstan$regExp%", $this->getActualOutput());
-        $this->assertRegExp("%parallel-lint$regExp%", $this->getActualOutput());
-        $this->assertRegExp('%Tiempo total de ejecución = \d+\.\d{2} sec%', $this->getActualOutput());
-        // $this->assertStringContainsString('phpcpd - OK. Time:', $this->getActualOutput());
-        // $this->assertStringContainsString('phpstan - OK. Time:', $this->getActualOutput());
-        // $this->assertStringContainsString('phpcs - OK. Time:', $this->getActualOutput());
-        // $this->assertStringContainsString('phpmd - OK. Time:', $this->getActualOutput());
-        // $this->assertStringContainsString('parallel-lint - OK. Time:', $this->getActualOutput());
-        // $this->assertStringContainsString('check-security - OK. Time:', $this->getActualOutput());
     }
 
     /** @test */
