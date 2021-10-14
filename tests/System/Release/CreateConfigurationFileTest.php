98ee5a9c73ccc41e6ac482df077c4df43f63986c
Wtyd <jorgelilaoc@gmail.com>
2021-10-14 16:55:57 +0000

    tests(release) CreateConfigurationFileTest

---
+++ tests/System/Release/CreateConfigurationFileTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\System\Release;
+
+use Tests\ReleaseTestCase;
+
+/**
+ * @group release
+ */
+class CreateConfigurationFileTest extends ReleaseTestCase
+{
+    /** @test */
+    function it_creates_the_configuration_file_and_returns_exit_code_0()
+    {
+        mkdir('vendor/wtyd/githooks/qa/', 0777, true);
+        file_put_contents(
+            'vendor/wtyd/githooks/qa/githooks.dist.yml',
+            $this->configurationFileBuilder->buildYalm()
+        );
+
+        passthru("$this->githooks conf:init", $exitCode);
+
+        $this->assertStringContainsString('Configuration file githooks.yml has been created in root path', $this->getActualOutput());
+        $this->assertEquals(0, $exitCode);
+        $this->assertFileExists('githooks.yml');
+
+        $this->deleteDirStructure('vendor/wtyd');
+    }
+}
