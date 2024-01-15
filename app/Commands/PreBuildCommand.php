0b4bf0a870db13b246af25d5ef6b27bc27cbc37d
Wtyd <jorgelilaoc@gmail.com>
2024-01-15 09:44:12 +0000

    refactor (PreBuildCommand) Added phpVersion argument and adapted to the new Composer location

--- app/Commands/PreBuildCommand.php
+++ app/Commands/PreBuildCommand.php
@@ -2,13 +2,27 @@
 
 namespace Wtyd\GitHooks\App\Commands;
 
-use Wtyd\GitHooks\Utils\Printer;
 use LaravelZero\Framework\Commands\Command;
 use Wtyd\GitHooks\Build\ManageDependencies;
+use Wtyd\GitHooks\Utils\Printer;
 
 class PreBuildCommand extends Command
 {
-    protected $signature = 'app:pre-build';
+    public const DEV_DEPENDENCIES = [
+        // 'intonate/tinker-zero', // No way to use build command without tinker
+        'mikey179/vfsstream',
+        'mockery/mockery',
+        'php-mock/php-mock',
+        'php-parallel-lint/php-parallel-lint',
+        'phpmd/phpmd',
+        'phpstan/phpstan',
+        'phpunit/php-code-coverage',
+        'phpunit/phpunit',
+        'squizlabs/php_codesniffer',
+        'fakerphp/faker',
+    ];
+
+    protected $signature = 'app:pre-build {phpVersion? : Version of php to use. Default is the current version.}';
     protected $description = 'Prepares the app dependencies for the build processs';
 
     /**
@@ -18,25 +32,37 @@ class PreBuildCommand extends Command
      */
     protected $help = 'Without arguments deletes the pre-commit hook. A optional argument can be the name of another hook. Example: hook:clean pre-push.';
 
-    /**
-     * @var Printer
-     */
-    protected $printer;
+    /**  @var string */
+    private $phpVersion;
 
-    /** @var ManageDependencies */
-    private $manageDependencies;
+    /**  @var integer */
+    private $deleteDevDependenciesExitCode = -1;
 
-    public function __construct(ManageDependencies $manageDependencies)
-    {
-        parent::__construct();
-        $this->manageDependencies = $manageDependencies;
-    }
+    /**  @var integer */
+    private $updateProdDependenciesExitCode = -1;
+
+    private $composer = 'tools/composer';
+
     public function handle()
     {
+        $this->phpVersion = $this->argument('phpVersion') ?? 'php7.1';
+
         $this->title('Deleting dev dependencies');
-        $this->manageDependencies->deleteDevDependencies();
+        $this->deleteDevDependencies();
 
         $this->title('Updating prod dependencies');
-        $this->manageDependencies->updateProdDependencies();
+        $this->updateProdDependencies();
+    }
+
+    public function deleteDevDependencies(): void
+    {
+        $command = $this->phpVersion . ' ' . $this->composer . ' remove --ansi --dev ' . implode(' ', self::DEV_DEPENDENCIES);
+        passthru($command, $this->deleteDevDependenciesExitCode);
+    }
+
+    public function updateProdDependencies(): void
+    {
+        $command = $this->phpVersion . ' ' . $this->composer . ' update --ansi';
+        passthru($command, $this->updateProdDependenciesExitCode);
     }
 }
