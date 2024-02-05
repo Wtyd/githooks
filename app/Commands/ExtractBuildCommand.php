c071226f11a93036b406cd0fa8556af6dc3c0e4f
Wtyd <jorgelilaoc@gmail.com>
2024-02-05 22:53:37 +0000

    Fix

--- app/Commands/ExtractBuildCommand.php
+++ app/Commands/ExtractBuildCommand.php
@@ -60,7 +60,7 @@ class ExtractBuildCommand extends Command
     private function extractBuild(): void
     {
         $newBuildOfActualPhpVersion = $this->build->getBuildPath() . $this->getBinary();
-        $zipFile = File::name($this->build->getTarName()) . '.zip' ;
+        $zipFile = File::name($this->build->getTarName());
         // dd($zipFile,$this->build->getTarName());
         $zip = new ZipArchive();
         if ($zip->open($zipFile) !== true) {
